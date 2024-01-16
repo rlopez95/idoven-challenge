0361cfad06d6aff372ebc41dfe5143fdf4acfb02
rlopez95 <lopezruben95@gmail.com>
2024-01-16 18:48:08 +0100

    fix not annotated fields in settings

--- idoven_app/idoven/config.py
+++ idoven_app/idoven/config.py
@@ -5,9 +5,9 @@ class Settings(BaseSettings):
     mongo_uri: str = "mongodb://mongo:27017/ecg"
     postgres_uri: str = "postgresql://postgres:postgres@localhost/postgres"
     api_v1_prefix: str = "/api/v1"
-    jwt_secret_key = "adba27e22c8cff65dd8e713bce6383c789eadcae6c95945ceab3c94a2697b8d9"
-    jwt_algorith = "HS256"
-    token_expiration_time = 30
+    jwt_secret_key: str = "adba27e22c8cff65dd8e713bce6383c789eadcae6c95945ceab3c94a2697b8d9"
+    jwt_algorith: str = "HS256"
+    token_expiration_time: int = 30
 
 
 settings = Settings()
