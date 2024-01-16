75a51aadabfa9e6d969428d85b32e43ba4b8495e
rlopez95 <lopezruben95@gmail.com>
2024-01-16 21:22:52 +0100

    fix postgres uri to point container

--- idoven_app/idoven/config.py
+++ idoven_app/idoven/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     mongo_uri: str = "mongodb://mongo:27017/ecg"
-    postgres_uri: str = "postgresql://postgres:postgres@localhost/postgres"
+    postgres_uri: str = "postgresql://postgres:postgres@postgres:5432/postgres"
     api_v1_prefix: str = "/api/v1"
     jwt_secret_key: str = "adba27e22c8cff65dd8e713bce6383c789eadcae6c95945ceab3c94a2697b8d9"
     jwt_algorith: str = "HS256"
