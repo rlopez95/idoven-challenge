65b83ff07057644000966921caf2bf3fcca175a6
rlopez95 <lopezruben95@gmail.com>
2024-01-16 18:43:13 +0100

    updated user domain model

--- idoven_app/idoven/domain/user.py
+++ idoven_app/idoven/domain/user.py
@@ -1,6 +1,9 @@
 import uuid
 from enum import StrEnum
 from dataclasses import dataclass, asdict
+from passlib.context import CryptContext
+
+_pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class Role(StrEnum):
@@ -12,8 +15,11 @@ class Role(StrEnum):
 class User:
     user_id: str
     username: str
-    password: str
+    hashed_password: str
     role: Role
+
+    def verify_password(self, plain_password: str) -> bool:
+        return _pwd_context.verify(plain_password, self.hashed_password)
 
     def to_dict(self) -> dict:
         return asdict(self)
@@ -36,8 +42,12 @@ class UserFactory:
         if not role or role not in Role.__members__.values():
             raise UserInvalidException("User must have a valid role")
 
-        return User(user_id=user_id, username=username, password=password, role=role)
+        return User(user_id=user_id, username=username, password=_pwd_context.hash(password), role=role)
 
 
 class UserInvalidException(Exception):
+    pass
+
+
+class UserNotFoundException(Exception):
     pass
