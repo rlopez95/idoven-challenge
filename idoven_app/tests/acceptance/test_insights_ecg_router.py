cc84bdf045ca6d3798c1196bf3ca67bd9ff9cc73
rlopez95 <lopezruben95@gmail.com>
2024-01-17 02:41:38 +0100

    updated test to include auth

--- idoven_app/tests/acceptance/test_insights_ecg_router.py
+++ idoven_app/tests/acceptance/test_insights_ecg_router.py
@@ -9,8 +9,11 @@ client = TestClient(app)
 
 
 def test_get_ecg_insights():
+    auth_response = client.post("/api/v1/token", data={"username": "daviddoe", "password": "pwd"})
+    token = auth_response.json()["access_token"]
+
     ecg_id = "60d7c1318e1e0a241c4b4d9e"
-    response = client.get(f"/api/v1/insights/{ecg_id}")
+    response = client.get(f"/api/v1/insights/{ecg_id}", headers={"Authorization": f"Bearer {token}"})
 
     assert response.status_code == status.HTTP_200_OK
     insights_json = response.json()
@@ -18,9 +21,14 @@ def test_get_ecg_insights():
 
 
 def test_get_non_existing_ecg_insights_returns_not_found_error():
+    auth_response = client.post("/api/v1/token", data={"username": "daviddoe", "password": "pwd"})
+    token = auth_response.json()["access_token"]
     ecg_id = str(ObjectId())
-    response = client.get(f"/api/v1/insights/{ecg_id}")
+    response = client.get(
+        f"/api/v1/insights/{ecg_id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
     insights_json = response.json()
-    assert insights_json["detail"] == f"ECG {ecg_id} not found"
+    assert insights_json["detail"] == f"ECG {ecg_id} not found for current user daviddoe"
--- idoven_app/tests/acceptance/test_register_ecg_router.py
+++ idoven_app/tests/acceptance/test_register_ecg_router.py
@@ -8,6 +8,9 @@ client = TestClient(app)
 
 
 def test_register_a_ecg() -> None:
+    auth_response = client.post("/api/v1/token", data={"username": "johndoe", "password": "password1"})
+    token = auth_response.json()["access_token"]
+
     ecg_id = str(ObjectId())
     register_response = client.post(
         "/api/v1/register",
@@ -16,22 +19,30 @@ def test_register_a_ecg() -> None:
             "date": str(TestECGData.ANY_DATE),
             "leads": [lead.to_dict() for lead in TestECGData.ANY_LEADS],
         },
+        headers={"Authorization": f"Bearer {token}"},
     )
 
     assert register_response.status_code == status.HTTP_201_CREATED
 
-    insight_response = client.get(f"/api/v1/insights/{ecg_id}")
+    insight_response = client.get(f"/api/v1/insights/{ecg_id}", headers={"Authorization": f"Bearer {token}"})
     assert insight_response.status_code == status.HTTP_200_OK
 
 
 def test_register_a_ecg_with_no_leads_returns_bad_request() -> None:
+    auth_response = client.post("/api/v1/token", data={"username": "johndoe", "password": "password1"})
+    token = auth_response.json()["access_token"]
+
     invalid_leads = []
     payload = {
         "ecg_id": TestECGData.ANY_ECG_ID,
         "date": str(TestECGData.ANY_DATE),
         "leads": invalid_leads,
     }
-    response = client.post("/api/v1/register", json=payload)
+    response = client.post(
+        "/api/v1/register",
+        json=payload,
+        headers={"Authorization": f"Bearer {token}"},
+    )
 
     ecg_json = response.json()
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -42,13 +53,20 @@ def test_register_a_ecg_with_no_leads_returns_bad_request() -> None:
 
 
 def test_register_a_ecg_with_invalid_ecg_id_returns_bad_request() -> None:
+    auth_response = client.post("/api/v1/token", data={"username": "johndoe", "password": "password1"})
+    token = auth_response.json()["access_token"]
+
     invalid_id = "invalid-ecg-id"
     payload = {
         "ecg_id": invalid_id,
         "date": str(TestECGData.ANY_DATE),
         "leads": [lead.to_dict() for lead in TestECGData.ANY_LEADS],
     }
-    response = client.post("/api/v1/register", json=payload)
+    response = client.post(
+        "/api/v1/register",
+        json=payload,
+        headers={"Authorization": f"Bearer {token}"},
+    )
 
     ecg_json = response.json()
     assert response.status_code == status.HTTP_400_BAD_REQUEST
