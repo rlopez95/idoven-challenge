4caac5790ac1e2c2aea307cd36c547c65b490a04
rlopez95 <lopezruben95@gmail.com>
2024-01-17 10:51:30 +0100

    use .env for config settings

--- idoven_app/idoven/config.py
+++ idoven_app/idoven/config.py
@@ -2,12 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    mongo_uri: str = "mongodb://mongo:27017/ecg"
-    postgres_uri: str = "postgresql://postgres:postgres@postgres:5432/postgres"
-    api_v1_prefix: str = "/api/v1"
-    jwt_secret_key: str = "adba27e22c8cff65dd8e713bce6383c789eadcae6c95945ceab3c94a2697b8d9"
-    jwt_algorith: str = "HS256"
+    mongo_uri: str
+    postgres_uri: str
+    api_v1_prefix: str
+    jwt_secret_key: str
+    jwt_algorith: str
     token_expiration_time: int = 30
+    
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
